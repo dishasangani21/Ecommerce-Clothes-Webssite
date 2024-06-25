ed4ce895c5e0ff5dda5e085ac8e431c804524555
dishasanganu21 <dishasangani2106@gmail.com>
2024-06-25 15:53:20 +0530

    first commit

---
+++ e-commerce-admin/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Copyright by Disha Sangani</p>
+    </div>
+  )
+}
+
+export default Footer
